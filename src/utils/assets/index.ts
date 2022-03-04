26462c80f031e349d5894eda4c24683461f9c5db
강은빈 <kub0803@naver.com>
2022-03-04 17:06:39 +0900

    [FEAT] icon images 추가

---
+++ src/utils/assets/index.ts
@@ -0,0 +1,8 @@
+export { default as Logo } from "./icon/Logo.svg";
+export { default as MainBack } from "./img/mainBack.svg";
+export { default as Calendar } from "./icon/calendar.svg";
+export { default as Close } from "./icon/close.svg";
+export { default as NavHomeIcon } from "./icon/NavHomeIcon.png";
+export { default as NavCalendarIcon } from "./icon/NavCalendarIcon.svg";
+export { default as PickMainLogo } from "./img/PickMain.svg";
+export { default as Pick } from "../assets/icon/pick.png";
