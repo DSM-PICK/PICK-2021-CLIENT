c3521bed69b03375c94ec2a46778b93413b6a7c2
LeeJinWoo <wlsdn5793@gmail.com>
2022-03-07 23:39:46 +0900

    [FEAT] 학생 필터 및 출석 조회 퍼블리싱

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,11 +3,12 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import SplashScreen from "../components/common/SplashScreen";
 import MainPage from "../page";
 import Login from "../page/Login";
-
+import AttendanceInquiry from "../components/copyComponents/attendanceInquiry/AttendanceInquiry";
 const Router: FC = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/atI" element={<AttendanceInquiry />} />
         <Route path="/" element={<SplashScreen />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<MainPage />} />
