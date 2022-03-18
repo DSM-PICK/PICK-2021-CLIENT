0ceae6fb75953d4c0b7f738ae64751140294532b
LeeJinWoo <wlsdn5793@gmail.com>
2022-03-18 19:28:59 +0900

    [FEAT] Filter Recoil 추가

--- src/components/desktop/AttendanceCheck/Filter.tsx
+++ src/components/desktop/AttendanceCheck/Filter.tsx
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import * as S from "./styles";
-const studentFilter: string[] = ["전체", "1학년", "2학년", "3학년"];
-const statusFilter: string[] = ["전체", "외출", "현체", "귀가", "무단", "취업"];
+import { useRecoilState } from "recoil";
+import {
+  statusFilter,
+  studentFilter,
+} from "../../../modules/desktop/atom/ATCheck";
+const studentArr: string[] = ["전체", "1학년", "2학년", "3학년"];
+const statusArr: string[] = ["전체", "외출", "현체", "귀가", "무단", "취업"];
+
 const Filter = () => {
-  const [stuFilterValue, setStuFilterValue] = useState("전체");
-  const [statusFilterValue, setStatusFilterValue] = useState("전체");
+  const [stuFilterValue, setStuFilterValue] = useRecoilState(studentFilter);
+  const [statusFilterValue, setStatusFilterValue] =
+    useRecoilState(statusFilter);
 
   const onClick = (isStu: boolean, text: string) => {
     isStu ? setStuFilterValue(text) : setStatusFilterValue(text);
   };
+
   return (
     <S.Filter>
       <S.FilterTitle>학생별</S.FilterTitle>
       <S.StatusFilterBox isBig={false}>
-        {studentFilter.map((filter, i) => (
+        {studentArr.map((filter, i) => (
           <S.Btn
             onClick={() => onClick(true, filter)}
             key={i}
@@ -26,7 +34,7 @@ const Filter = () => {
 
       <S.FilterTitle>상태별</S.FilterTitle>
       <S.StatusFilterBox isBig={true}>
-        {statusFilter.map((filter, i) => (
+        {statusArr.map((filter, i) => (
           <S.Btn
             onClick={() => onClick(false, filter)}
             key={i}
