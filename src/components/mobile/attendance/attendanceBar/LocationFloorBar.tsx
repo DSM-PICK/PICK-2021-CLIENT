4e74b90dd0940d06cfdff199fed26fcb74a87864
강은빈 <kub0803@naver.com>
2022-04-13 00:01:17 +0900

    [FEAT] LoactionFloorBar 추가

---
+++ src/components/mobile/attendance/attendanceBar/LocationFloorBar.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  FloorBtnList,
+  StateBtnType,
+} from "../../../../lib/interface/mobile/Attendance/StateBtnType";
+import { LocationType } from "../../../../lib/interface/mobile/location";
+import SelectItem from "./SelectItem";
+import { SelectBarWrapper } from "./style";
+
+const LocationFloorBar = () => {
+  const navigate = useNavigate();
+  const [selected, setSelected] = useState<number>(2);
+
+  const selectedHandlerColor = (item: LocationType) => {
+    localStorage.setItem("locationId", String(item?.id));
+    setSelected(item.id);
+  };
+
+  useEffect(() => {
+    navigate(`/attendance?floor=${selected}&id=`);
+  }, [navigate, selected]);
+
+  return (
+    <SelectBarWrapper>
+      {FloorBtnList.map((item: StateBtnType) => (
+        <SelectItem
+          key={item.id}
+          item={item}
+          selected={selected}
+          selectedHandlerColor={selectedHandlerColor}
+        />
+      ))}
+    </SelectBarWrapper>
+  );
+};
+
+export default LocationFloorBar;
